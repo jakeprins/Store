59022d17ee09d88f75b7dc97427f6be5fdcbdb74
jakeprins <jakeprins@outlook.com>
2016-04-18 17:48:31 +0200

    Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418144242) do
+ActiveRecord::Schema.define(version: 20160418154628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer  "inventory_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "name"
+  end
+
+  add_index "categories", ["inventory_id"], name: "index_categories_on_inventory_id", using: :btree
 
   create_table "inventories", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +33,11 @@ ActiveRecord::Schema.define(version: 20160418144242) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "category_id"
   end
 
+  add_index "inventories", ["category_id"], name: "index_inventories_on_category_id", using: :btree
+
+  add_foreign_key "categories", "inventories"
+  add_foreign_key "inventories", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,40 @@ inventory = [
   ["Electric keyboard", "This keyboard is capable of recreating a wide range of
     instrument sounds (piano, electric piano, Hammond organ, pipe organ, violin, etc.)
     with less complex sound synthesis. Electronic keyboards are usually dedicated for
-    home users, beginners and other non-professional users.", 550, 5, ""],
+    home users, beginners and other non-professional users.", 550, 5, "", "Piano's"],
 
   ["Grand piano", "A big piano with the frame and strings horizontal.
     The strings extending away from the keyboard. The action lies beneath
     the strings, and uses gravity as its means of return to a state of rest.",
-     2200, 2, ""],
+     2200, 2, "", "Piano's"],
 
-  ["Classic guitar", "Spanish classi guitar for a warm and southren sound", 300, 4],
+  ["Classic guitar", "Spanish classi guitar for a warm and southren sound", 300, 4, "", "Guitars"],
 
-  ["Drumsticks", "High quality drumsticks. Best in the world!", 10, 10, ""],
+  ["Drumsticks", "High quality drumsticks. Best in the world!", 10, 10, "", "Drums"],
 
-  ["Stradivarius Violin", "The fine and extreme expensive violin. Best in the world!", 10000, 10, ""],
+  ["Stradivarius Violin", "The fine and extreme expensive violin. Best in the world!", 10000, 10, "", "Violins"],
 
-  ["Yamaha Violin", "Good quality violin", 350, 10, ""],
+  ["Yamaha Violin", "Good quality violin", 350, 10, "", "Violins"],
 
-  ["Pearl Drumset", "High quality drumset. Best in the world!", 800, 10, ""]
+  ["Pearl Drumset", "High quality drumset. Best in the world!", 800, 10, "", "Drums"]
 ]
 
-inventory.each do |name, description, price, quantity, image|
+categories = [
+  "Piano's",
+  "Guitars",
+  "Drums",
+  "Violins"
+]
+
+inventory.each do |name, description, price, quantity, image, categories|
   Inventory.create(name: name, description: description, price: price, quantity: quantity, image: image)
+
+  categories.split( ", ").each do |category_name|
+    if
+      category = Category.find_by( name: category_name)
+    else
+      category = Category.create( name: category_name)
+    end
+
+  end
 end
