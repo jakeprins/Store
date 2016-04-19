b36216d184b3e68a82b1889760016b56a4ec4b3c
jakeprins <jakeprins@outlook.com>
2016-04-19 16:52:43 +0200

    index inventory

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160418154628) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,20 +36,9 @@ ActiveRecord::Schema.define(version: 20160418154628) do
     t.integer  "category_id"
   end
 
-
   add_index "inventories", ["category_id"], name: "index_inventories_on_category_id", using: :btree
 
-  add_foreign_key "categories", "inventories"
-  add_foreign_key "inventories", "categories"
-
   create_table "users", force: :cascade do |t|
-    t.string   "user_name"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "adress_name"
-    t.string   "adress_number"
-    t.string   "city"
-    t.string   "country"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -60,8 +47,8 @@ ActiveRecord::Schema.define(version: 20160418154628) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
@@ -69,4 +56,6 @@ ActiveRecord::Schema.define(version: 20160418154628) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "categories", "inventories"
+  add_foreign_key "inventories", "categories"
 end
