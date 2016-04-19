d5dc4d1feeadfb42064332fd1028123ea9bbbb90
iulia0103 <iulia.leancu@gmail.com>
2016-04-19 14:15:13 +0200

    refined header and pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,22 @@ inventory = [
   ["Electric keyboard", "This keyboard is capable of recreating a wide range of
     instrument sounds (piano, electric piano, Hammond organ, pipe organ, violin, etc.)
     with less complex sound synthesis. Electronic keyboards are usually dedicated for
-    home users, beginners and other non-professional users.", 550, 5, ""],
+    home users, beginners and other non-professional users.", 550, 5, "keyboard.jpg"],
 
   ["Grand piano", "A big piano with the frame and strings horizontal.
     The strings extending away from the keyboard. The action lies beneath
     the strings, and uses gravity as its means of return to a state of rest.",
-     2200, 2, ""],
+     2200, 2, "piano.jpg"],
 
-  ["Classic guitar", "Spanish classi guitar for a warm and southren sound", 300, 4],
+  ["Classic guitar", "Spanish classi guitar for a warm and southren sound", 300, 4, "guitar.jpg"],
 
-  ["Drumsticks", "High quality drumsticks. Best in the world!", 10, 10, ""],
+  ["Drumsticks", "High quality drumsticks. Best in the world!", 10, 10, "drumsticks.jpg"],
 
-  ["Stradivarius Violin", "The fine and extreme expensive violin. Best in the world!", 10000, 10, ""],
+  ["Stradivarius Violin", "The fine and extreme expensive violin. Best in the world!", 10000, 10, "violin.jpg"],
 
-  ["Yamaha Violin", "Good quality violin", 350, 10, ""],
+  ["Yamaha Violin", "Good quality violin", 350, 10, "yamaha_violin.jpg"],
 
-  ["Pearl Drumset", "High quality drumset. Best in the world!", 800, 10, ""]
+  ["Pearl Drumset", "High quality drumset. Best in the world!", 800, 10, "drums.jpg"]
 ]
 
 inventory.each do |name, description, price, quantity, image|
