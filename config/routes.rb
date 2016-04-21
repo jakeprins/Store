8105868c70538e2283725ddf35b28d8005aff941
jakeprins <jakeprins@outlook.com>
2016-04-21 17:04:05 +0200

    Refactor Inventory to Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :inventory
+  resources :products
   resources :categories
 
   get "/shopping_cart_items" => "shopping_cart_items#index"
   get "/shopping_cart_items/clear" => "shopping_cart_items#clear"
   get "/shopping_cart_items/:id" => "shopping_cart_items#add"
 
-  root 'inventory#welcome'
+  root 'products#welcome'
 
 end
