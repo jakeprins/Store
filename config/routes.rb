c2b0f457c6678e877194bbb5bcb54902a0cea0f9
jakeprins <jakeprins@outlook.com>
2016-04-22 14:55:13 +0200

    Contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
 
+  match '/contacts',  to: 'contacts#new',          via: 'get'
+  resources "contacts", only: [:new, :create]
+
   get "/shopping_cart_items" => "shopping_cart_items#index"
   get "/shopping_cart_items/clear" => "shopping_cart_items#clear"
   get "/shopping_cart_items/:id" => "shopping_cart_items#add"
 
   root 'products#welcome'
+
+
+
 
 end
